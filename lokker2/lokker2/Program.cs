05f06daf2f05508a35a5934b281e995808d26baf
Renee <97166586+reneeatho@users.noreply.github.com>
2022-05-06 12:39:45 +0200

    Add files via upload

---
+++ lokker2/lokker2/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+class Program
+{
+    static void Main(string[] args)
+    {
+     
+        }
+    private void ForEach()
+    {
+        string [] numberArray = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+        foreach (var number int in numberArray)
+                {
+            Console.WriteLine(number);
+        }
+    }
+}
