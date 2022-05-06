d5cb917d69d62feec2841976ace185a9a6e29e61
Renee <97166586+reneeatho@users.noreply.github.com>
2022-05-06 12:38:49 +0200

    Add files via upload

---
+++ Mainmetodenogargs2/Mainmetodenogargs2/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("God mat er godt");
+        
+
+
+
+    }
+
+}
