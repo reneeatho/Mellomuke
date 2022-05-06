dd869bc7135ada8d05af77e45291b36deb9b27ae
Renee <97166586+reneeatho@users.noreply.github.com>
2022-05-06 12:40:43 +0200

    Add files via upload

---
+++ Mainmetodenogargs1/Mainmetodenogargs1/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Du er en kråke!");
+        Console.WriteLine("Jeg er sterkest!");
+        Console.WriteLine("Jeg eier et hus!");
+        Console.WriteLine("Saken er biff!");
+        Console.WriteLine("Du kledde den kjolen!");
+
+        Console.ReadLine();
+    }
+}
