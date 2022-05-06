30b4f97786b3be0a7332aac6c66ed059f012a070
Renee <97166586+reneeatho@users.noreply.github.com>
2022-05-06 12:39:26 +0200

    Add files via upload

---
+++ lokker1/lokker1/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+{
+
+  bool condition = true;
+        int i = 0;
+        while (condition)
+        {
+            if (i < 5)
+            {
+                Console.WriteLine("Terje er Kul");
+                i++;
+            }
+            else 
+            {
+                condition = false; 
+            }
+        }
+    }
+
+    Console.ReadLine();
+
+
+
+
+
