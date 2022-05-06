f24d167ed17fd7bceabef706ff4d4443f18546d6
Renee <97166586+reneeatho@users.noreply.github.com>
2022-05-06 12:41:12 +0200

    Add files via upload

---
+++ ifElse1/ifElse1/Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+Console.WriteLine("Skriv inn ett tall");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Skriv inn ett annet tall");
+int b = Convert.ToInt32(Console.ReadLine());
+
+
+if (a != b) ;
+{
+    Console.WriteLine("Tallene er IKKE like: " + (a + b));
+}
+
+else
+{
+    Console.WriteLine("Tallene er like: " + (a * b));
+
+}
+
+if (a == 30  b == 30  a + b == 30);
+{
+    Console.WriteLine("True");
+}
+
+else
+{
+    Console.WriteLine("False");
+}
